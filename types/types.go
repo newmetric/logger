8a5e175f0be3b4dc19a5bd9441202df6a8d831dc
Jiseok CHOI <jiseok.dev@gmail.com>
2023-04-26 17:13:39 +0900

    feat: add `Fatal` method in `Logger` (#5)

--- types/types.go
+++ types/types.go
@@ -4,9 +4,11 @@ type Logger interface {
 	Level(level string) error
 	With(args ...interface{}) Logger
 
+	// sort by log level
 	Debug(msg string, args ...interface{})
 	Info(msg string, args ...interface{})
 	Warn(msg string, args ...interface{})
 	Error(msg string, args ...interface{})
+	Fatal(msg string, args ...interface{})
 	Trace(msg string, args ...interface{})
 }
