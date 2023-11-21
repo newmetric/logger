38ab26e78a8d4dc3bc8d4a982aac79d76bdb4e24
Jiseok CHOI <jiseok.dev@gmail.com>
2023-11-21 17:19:48 +0900

    todo/nm: add fmt

--- logger/nm/nm.go
+++ logger/nm/nm.go
@@ -1,3 +1,7 @@
 package nm
 
-// TODO:
+// 2023-11-21T08:17:40.897668Z  INFO <module>: <msg>
+// 2023-11-21T08:17:40.897743Z DEBUG <module>: <msg>
+// 2023-11-21T08:17:40.897753Z  WARN <module>: <msg>
+// 2023-11-21T08:17:40.897762Z ERROR <module>: <msg>
+// 2023-11-21T08:17:40.897771Z TRACE <module>: <msg>
