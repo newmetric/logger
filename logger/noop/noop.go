bf51257927063672cf28041a5f8e292f9101aace
Jiseok CHOI <jiseok.dev@gmail.com>
2024-08-23 16:52:05 +0900

    feat: add printf

--- logger/noop/noop.go
+++ logger/noop/noop.go
@@ -1,8 +1,9 @@
 package noop
 
 import (
-	"github.com/newmetric/logger/types"
 	"io"
+
+	"github.com/newmetric/logger/types"
 )
 
 type NoOpLogger struct{}
@@ -15,6 +16,8 @@ func (n *NoOpLogger) With(args ...interface{}) types.Logger { return n }
 
 func (n *NoOpLogger) SetLevel(types.Level) error { return nil }
 func (n *NoOpLogger) GetLevel() types.Level      { return types.Disabled }
+
+func (n *NoOpLogger) Printf(format string, args ...interface{}) {}
 
 func (n *NoOpLogger) Debug(msg string, args ...interface{}) {}
 func (n *NoOpLogger) Info(msg string, args ...interface{})  {}
